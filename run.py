f4ac16d1cbe37a6c92b71501de27b2337ff9cd1a
Rahul Bhattacharjee <rahuldecoded>
2017-10-16 20:08:27 +0530

    Modification done in run.py and replies.py in added
    	* changed the bot-name
    
    	* removed unnecessary code

--- run.py
+++ run.py
@@ -1,19 +1,9 @@
 import re
 import socket
-from replies import reply, find_in_Replies
-from Queue import Queue
-import traceback
-from weather import weather
-from bs4 import BeautifulSoup
-from joke import joke
-from google import get_urls
-import wiki
 
-user_queue = Queue()
-'''
-alg = algebra()
-# For sending messages to a specified channel
-'''
+import traceback
+
+from replies import reply, find_in_Replies
 
 
 def sendmsg(chan, msg):
@@ -36,14 +26,14 @@ def ping():
 # This is a main routine
 def main():
     global irc, user_queue
-    botnick = "pikachu_"
+    botnick = "nutrient-bot"
     bufsize = 2048
     admin = ["rahuldecoded"]
     channel = "#uit-foss"
     port = 6667
     server = "irc.freenode.net"
     master = "rahuldecoded"
-    uname = "Pikachu - Test Bot"
+    uname = "Nutrient Bot"
     realname = "I'm a Test Bot!"
     irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     irc.connect((server, port))
@@ -88,22 +78,6 @@ def main():
             request = tokens[3]
             tokenLength = len(tokens)
 
-            if request == "::show":
-                if ircmsg.strip(":").split("!")[0] in admin:
-                    sendmsg(channel, str(user_queue.in_queue()) + " \n")
-
-            # For getting to the next user in the queue.
-            if request == "::next":
-                if ircmsg.strip(":").split("!")[0] in admin:
-                    sendmsg(channel, str(user_queue.next_nick()) + " \n")
-                    user_queue.pop_next()
-
-            # For clearing the queue.
-            if request == "::clearqueue":
-                if ircmsg.strip(":").split("!")[0] in admin:
-                    user_queue.clear()
-                    sendmsg(channel, "Queue cleared")
-
             # For adding someone as a admin
             if request == "::add":
                 if tokenLength > 4:
@@ -123,39 +97,10 @@ def main():
                     sendmsg(channel, "Usage: :remove [nick]")
 
             # User Commands
-            if request == ":!":
-                user_name = ircmsg.strip(":").split("!")
-                sendmsg(channel, str(user_name[0]) + " , you have added in queue. Wait for your turn.\n")
-                user_queue.enqueue(user_name[0])
-
-            # Command for temperature
-            # Syntax: ":temp kolkata"
-            if request == "::temp":
-                if tokenLength > 4:
-                    sendmsg(channel, weather(tokens[1]))
-                elif tokenLength == 4:
-                    sendmsg(channel, "Usage: :temp [city name]")
-
-            # Command for joke
-            # Syntax: ":joke"
-            if request == "::joke":
-                sendmsg(channel, joke())
-
-            # Command for google
-            # Syntax: ":google"
-            if request == "::google":
-                if tokenLength > 4:
-                    sendmsg(channel, get_urls(' '.join(tokens[4:])))
-                elif tokenLength == 4:
-                    sendmsg(channel, "Usage: :google [query]")
             
-            # Command for wiki
-            # Syntax: ":wiki"
-            if request == "::wiki": 
-                if tokenLength > 4:
-                    sendmsg(channel, wiki.summary(' '.join(tokens[4:])))
-                elif tokenLength == 4:
-                    sendmsg(channel, "Usage: :wiki [query]")
+
+
+            
 
         except Exception as e:
             tb = traceback.format_exc()
